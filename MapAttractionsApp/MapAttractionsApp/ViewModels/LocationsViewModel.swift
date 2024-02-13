20d5f6ed9d16156237fd4a3459fbef080c09cbdc
epdsant <epdsant@gmail.com>
2024-02-13 16:33:56 +0000

    feat: Added location preview cards with asymmetric Transitions

--- MapAttractionsApp/MapAttractionsApp/ViewModels/LocationsViewModel.swift
+++ MapAttractionsApp/MapAttractionsApp/ViewModels/LocationsViewModel.swift
@@ -59,4 +59,30 @@ class LocationsViewModel: ObservableObject{
             showLocationsList = false
         }
     }
+    
+    func nextButtonPressed() {
+        
+        //Get the current index
+        guard let currentIndex = locations.firstIndex(where: {$0 == mapLocation}) else {
+            print("OPS!, Could not find current index in locations array!")
+            return
+        }
+        
+        //Chek if the currentIndex is valid
+        let nextIndex = currentIndex + 1
+        guard locations.indices.contains(nextIndex) else {
+            
+            // Next index is not valid
+            //Restart from 0 index
+            guard let firstLocation = locations.first else {return}
+            showNextLocation(location: firstLocation)
+            
+            return
+        }
+        
+        //Next index is valid
+        let nextLocation = locations[nextIndex]
+        showNextLocation(location: nextLocation)
+        
+    }
 }
